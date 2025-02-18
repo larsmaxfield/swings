678a42ae92f25be8538678ce902b731269ee6865
Lars Maxfield <maxfield.lars@gmail.com>
2025-02-18 11:23:22 +0100

    False relative build

---
+++ cas-viewer/_app/immutable/chunks/Dw8TQ5hZ.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_19r4il3)==null?void 0:s.base)??"/swings/cas-viewer";var e;const t=((e=globalThis.__sveltekit_19r4il3)==null?void 0:e.assets)??a;export{t as a,a as b};
